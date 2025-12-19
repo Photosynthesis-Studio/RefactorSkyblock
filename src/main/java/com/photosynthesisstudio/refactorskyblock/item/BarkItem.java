a63b8c3e0d0f0881bf487515634e7357b24da90e
Black_Moss <kytheminecraft@outlook.com>
2025-12-19 18:43:48 +0800

    啃秒树皮要5s,得1f饱 70%概率得1级缓慢30秒

--- src/main/java/com/photosynthesisstudio/refactorskyblock/item/BarkItem.java
+++ src/main/java/com/photosynthesisstudio/refactorskyblock/item/BarkItem.java
@@ -1,13 +1,26 @@
 package com.photosynthesisstudio.refactorskyblock.item;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 
 public class BarkItem extends Item {
     public BarkItem(Properties properties) {
         super(properties.food(new FoodProperties.Builder()
-                .nutrition(3)
-                .saturationModifier(1)
-                .build())
+                        .nutrition(3)
+                        .saturationModifier(1f)
+                        .build())
+                .component(DataComponents.CONSUMABLE,
+                        Consumable.builder()
+                                .consumeSeconds(5f)
+                                .onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(
+                                        MobEffects.SLOWNESS,
+                                        600,
+                                        0), 0.7f))
+                                .build())
         );
     }
 }
