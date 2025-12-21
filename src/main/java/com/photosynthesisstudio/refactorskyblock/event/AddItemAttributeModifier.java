7748e0184d75a640040d2d469ba723801365abea
Black_Moss <kytheminecraft@outlook.com>
2025-12-21 17:39:46 +0800

    添加物品属性

--- src/main/java/com/photosynthesisstudio/refactorskyblock/event/AddItemAttributeModifier.java
+++ src/main/java/com/photosynthesisstudio/refactorskyblock/event/AddItemAttributeModifier.java
@@ -14,6 +14,7 @@ import net.neoforged.neoforge.event.ItemAttributeModifierEvent;
 public class AddItemAttributeModifier {
     @SubscribeEvent
     public void addItemAttributeModifier(ItemAttributeModifierEvent event) {
+        tool(ModItems.BARK_WAND.get(), 1.0f, event);
         tool(ModItems.SIMPLE_AXE.get(), 0.75f, event);
         tool(ModItems.SIMPLE_HOE.get(), 1.0f, event);
         tool(ModItems.SIMPLE_PICKAXE.get(), 1.0f,  event);
